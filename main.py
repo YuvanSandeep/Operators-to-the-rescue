a3ba6a39d10761050dc3ef58140c0765f869023c
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-02-02 19:22:22 +0000

    learnt about alot of operators

---
+++ main.py
@@ -0,0 +1,30 @@
+num1 = 6389
+num2 = 65
+
+# addition
+result = num1 + num2
+print (f"{num1} + {num2} = {result}")
+
+# subtraction
+result = num1 - num2
+print (f"{num1} - {num2} = {result}")
+
+# multiplication
+result = num1 * num2
+print (f"{num1} * {num2} = {result}")
+
+# division
+result = num1 / num2
+print (f"{num1} / {num2} = {result}")
+
+# round division
+result = num1 // num2
+print (f"{num1} // {num2} = {result}")
+
+# power
+result = num1 ** num2
+print (f"{num1} to the power of {num2} = {result}")
+
+# modulus
+result = num1 % num2
+print (f"{num1} % {num2} = {result}")
---
+++ main2.py
@@ -0,0 +1,18 @@
+num1 = 48384
+num2 = 48294
+
+# ==
+result = num1 == num2
+print (f"{num1} == {num2} is {result}")
+
+# not ==
+result = num1 != num2
+print (f"{num1} is not == {num2} is {result}")
+
+# >
+result = num1 > num2
+print (f"{num1} > {num2} is {result}")
+
+# >=
+result = num1 >= num2
+print (f"{num1} >= {num2} is {result}")
