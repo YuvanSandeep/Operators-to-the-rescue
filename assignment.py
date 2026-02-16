b43e371e4b454975939346587c369cacf0fb1677
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-02-16 19:22:02 +0000

    assignment for the first one

---
+++ assignment.py
@@ -0,0 +1,42 @@
+x = 30
+y = 4
+
+result = x + y
+print (result)
+
+result = x - y
+print (result)
+
+result = x * y
+print (result)
+
+result = x / y
+print (result)
+
+result = x % y
+print (result)
+
+result = x ** y
+print (result)
+
+x = 20
+y = 10
+
+result = x == y
+print (result)
+
+result = x != y
+print (result)
+
+result = x >= y
+print (result)
+
+result = x > y
+print (result)
+
+result = x <= y
+print (result)
+
+result = x < y
+print (result)
+
