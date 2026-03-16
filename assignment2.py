95167e815575e6d87f538cb166a3cf5e6890238f
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-03-16 18:52:17 +0000

    assignment for the second one

---
+++ assignment2.py
@@ -0,0 +1,52 @@
+x = 5
+y = 15
+
+result = x & y
+print (result)
+
+result = x | y
+print (result)
+
+result = x ^ y
+print (result)
+
+result = x << y
+print (result)
+
+result = x >> y
+print (result)
+
+
+
+x += y
+print (x)
+x = 5
+
+x -= y
+print (x)
+x = 5
+
+x *= y
+print (x)
+x = 5
+
+x /= y
+print (x)
+x = 5
+
+x %= y
+print (x)
+x = 5
+
+x **= y
+print (x)
+x = 5
+
+
+
+
+if x is y:
+    result = "Same"
+elif x is not y:
+    result = "Different"
+print (result)
