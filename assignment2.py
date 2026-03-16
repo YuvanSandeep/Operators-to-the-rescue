18bd4601132cae973891a9af87e03139b82360bd
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-03-16 18:53:37 +0000

    assignment for the second one fixed

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,7 @@
 x = 5
 y = 15
 
+# bitwise
 result = x & y
 print (result)
 
@@ -17,7 +18,7 @@ result = x >> y
 print (result)
 
 
-
+# assignment
 x += y
 print (x)
 x = 5
@@ -44,7 +45,7 @@ x = 5
 
 
 
-
+# identity
 if x is y:
     result = "Same"
 elif x is not y:
